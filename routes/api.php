1535ae820bf486208c3d5f121f1b61a69e919c28
GIP ATGeRi <gipatgeri@mbp-de-gip.home>
2021-04-30 11:23:17 +0200

    naming login

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::group([
     'prefix' => 'auth'
 
 ], function ($router) {
-    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
